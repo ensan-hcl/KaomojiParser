5b1d480f197cee962d0597da61ed4788ee9250d9
ensan <ensan.hcl.dev@gmail.com>
2021-08-22 23:30:12 +0900

    add test cases

--- Tests/KaomojiParserTests/KaomojiParserTests.swift
+++ Tests/KaomojiParserTests/KaomojiParserTests.swift
@@ -5,5 +5,7 @@
         func testParser() {
             let parser = KaomojiParser()
             XCTAssertEqual(parser.search(from: "嬉しいです(≧▽≦)", G: 3, L: 3), ["(≧▽≦)"])
+            XCTAssertEqual(parser.search(from: "地震だ！┗(^o^;)┓", G: 3, L: 3), ["┗(^o^;)┓"])
+            XCTAssertEqual(parser.search(from: "またね(・Д・)ノ", G: 3, L: 3), ["(・Д・)ノ"])
         }
     }
