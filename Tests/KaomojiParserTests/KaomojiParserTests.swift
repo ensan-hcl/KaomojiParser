f657bb27ee17f544349e4d254945ba49c8861308
ensan <ensan.hcl.dev@gmail.com>
2021-08-25 11:29:14 +0900

    improve parser and add some tests

--- Tests/KaomojiParserTests/KaomojiParserTests.swift
+++ Tests/KaomojiParserTests/KaomojiParserTests.swift
@@ -2,10 +2,19 @@
     @testable import KaomojiParser
 
     final class KaomojiParserTests: XCTestCase {
+        func testHalfKatakana() {
+            let parser = KaomojiParser()
+            XCTAssertEqual(parser.isHalfKatakana("ﾊ"), true)
+            XCTAssertEqual(parser.isHalfKatakana("レ"), false)
+            XCTAssertEqual(parser.isHalfKatakana("a"), false)
+        }
+
         func testParser() {
             let parser = KaomojiParser()
             XCTAssertEqual(parser.search(from: "嬉しいです(≧▽≦)", G: 3, L: 3), ["(≧▽≦)"])
             XCTAssertEqual(parser.search(from: "地震だ！┗(^o^;)┓", G: 3, L: 3), ["┗(^o^;)┓"])
             XCTAssertEqual(parser.search(from: "またね(・Д・)ノ", G: 3, L: 3), ["(・Д・)ノ"])
+            XCTAssertEqual(parser.search(from: "ホームランｷﾀ━━━━(ﾟ∀ﾟ)━━━━‼︎", G: 3, L: 3), ["ｷﾀ━━━━(ﾟ∀ﾟ)━━━━‼︎"])
+            XCTAssertEqual(parser.search(from: "嫌い(｀ε´)　絶交しよ( ￣っ￣)", G: 3, L: 3), ["(｀ε´)　", "( ￣っ￣)"])
         }
     }
